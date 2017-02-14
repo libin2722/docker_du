cf5dd8afb7afeb909fb1e1f87f1d34ef30d6cdf5
Terry.Li <libin2722@sohu.com>
2017-02-14 22:54:19 +0800

    ytkhyb

---
+++ ytkhyb/push.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker build -t hub.paladonsystems.com.cn/terry/ytkhyb .
+docker login --username=terry --password=111111 hub.paladonsystems.com.cn
+docker push hub.paladonsystems.com.cn/terry/ytkhyb
+
